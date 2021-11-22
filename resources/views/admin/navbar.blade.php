e7590dbaeb19f5fb795be0776942fefa2e1e9acc
Hammad Hassan <46231833+hammadshahir@users.noreply.github.com>
2021-11-22 12:00:14 +0100

    Added admin template

---
+++ resources/views/admin/navbar.blade.php
@@ -0,0 +1,43 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+      <a class="sidebar-brand brand-logo" href="#"><img src="admin/assets/images/logo.svg" alt="logo" /></a>
+      <a class="sidebar-brand brand-logo-mini" href="#"><img src="admin/assets/images/logo-mini.svg" alt="logo" /></a>
+    </div>
+    <ul class="nav">
+      <li class="nav-item nav-category">
+        <span class="nav-link">Navigation</span>
+      </li>
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="#">
+          <span class="menu-icon">
+            <i class="mdi mdi-account-star-outline"></i>
+          </span>
+          <span class="menu-title">User</span>
+        </a>
+      </li>
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="#">
+          <span class="menu-icon">
+            <i class="mdi mdi-food-croissant"></i>
+          </span>
+          <span class="menu-title">Food</span>
+        </a>
+      </li>
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="#">
+          <span class="menu-icon">
+            <i class="mdi mdi-chef-hat"></i>
+          </span>
+          <span class="menu-title">Chef</span>
+        </a>
+      </li>
+      <li class="nav-item menu-items">
+        <a class="nav-link" href="#">
+          <span class="menu-icon">
+            <i class="mdi mdi-calendar-range"></i>
+          </span>
+          <span class="menu-title">Reservations</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
