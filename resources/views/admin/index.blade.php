79e7eecb716d6c03223dd8ad6c8510224b7cc663
Hammad Hassan <46231833+hammadshahir@users.noreply.github.com>
2021-11-22 12:51:44 +0100

    optimised admin template

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,13 +4,13 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+    <head>
       @include("admin.header")
   </head>
   <body>
     <div class="container-scroller">
         @include("admin.navbar")
     </div>
-
+        @include("footer.navbar")
   </body>
 </html>
